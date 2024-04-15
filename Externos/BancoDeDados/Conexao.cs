29edbb59a699e790922cf8ef8c3542a2b1c6a85f
Tiago Dantas <tiagodantas42@gmail.com>
2024-04-15 16:12:41 -0300

    connect db initial

---
+++ Externos/BancoDeDados/Conexao.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reviewPlataformConsole.Externos.BancoDeDados
+{
+    public class Conexao
+    {
+        SqlConnection con = new SqlConnection();
+
+        //Construtor
+
+        public Conexao() {
+            con.ConnectionString = "";
+        }
+
+        //Metodo Conectar
+
+        public SqlConnection Conectar()
+        {
+
+        }
+
+        //Metodo Desconectar
+        public void Desconectar()
+        {
+
+        }
+
+    }
+}
